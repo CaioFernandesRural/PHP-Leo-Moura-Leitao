c6b95eed737744110c84e7ba741a2c480c61e762
caio <caioorebeo@gmail.com>
2022-12-20 20:04:08 -0300

    calculo e exibição de intervalos

--- innout/src/controllers/day_records.php
+++ innout/src/controllers/day_records.php
@@ -7,11 +7,5 @@ loadModel('WorkingHours');
 $date = (new DateTime())->getTimeStamp();
 $today = strftime('%d de %B de %Y', $date);
 
-$user = $_SESSION['user'];
-$records =
-    WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
-loadTemplateView('day_records', [
-    'today' => $today,
-    'records' => $records
-]);
+loadTemplateView('day_records', ['today' => $today,]);
--- innout/src/controllers/test.php
+++ innout/src/controllers/test.php
@@ -2,16 +2,16 @@
 
 //controller temporário
 
-$intervalo1 = DateInterval::createFromDateString('9 hours');
-$intervalo2 = DateInterval::createFromDateString('6 hours');
+loadModel('WorkingHours');
 
-$resultado1 = sumIntervals($intervalo1, $intervalo2);
-$resultado2 = subtractIntervals($intervalo1, $intervalo2);
+$wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
 
-print_r($resultado1);
+$wi = $wh->getWorkedInterval()->format('%H:%I:%S');
+print_r($wi);
 echo('<br>');
-print_r($resultado2);
+
+$li = $wh->getLunchInterval()->format('%H:%I:%S');
+print_r($li);
 echo('<br>');
-print_r(getDateFromInterval($resultado1));
-echo('<br>');
-print_r(getDateFromInterval($resultado2));
+
+print_r($wh->getExitTime());
