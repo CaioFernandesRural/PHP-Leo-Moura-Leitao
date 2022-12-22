604a37cd9a9a65918a7774a713b961894a1d3d65
caio <caioorebeo@gmail.com>
2022-12-21 22:20:18 -0300

    relógio funcionando em javascript

--- innout/src/views/template/left.php
+++ innout/src/views/template/left.php
@@ -31,19 +31,21 @@
         <div class="sidebar-widget">
             <i class="icon icofont-hour-glass text-primary"></i>
             <div class="info">
-                <span class="main text-primary">
-                   <?= $workedInterval ?>
-                </span>
-                <span class="label text-muted">
-                    Horas Trabalhadas
-                </span>
-            </div>
+                <span class="main text-primary"
+                <?= $activeClock == 'workedInterval' ? 'active-clock' : '' ?>>
+                <?= $workedInterval ?>
+            </span>
+            <span class="label text-muted">
+                Horas Trabalhadas
+            </span>
         </div>
-        <div class="division my-3"></div>
-        <div class="sidebar-widget">
-            <i class="icon icofont-ui-alarm text-danger"></i>
-            <div class="info">
-                <span class="main text-danger">
+    </div>
+    <div class="division my-3"></div>
+    <div class="sidebar-widget">
+        <i class="icon icofont-ui-alarm text-danger"></i>
+        <div class="info">
+            <span class="main text-danger"
+            <?= $activeClock == 'exitTime' ? 'active-clock' : '' ?>>
                     <?= $exitTime ?>
                 </span>
                 <span class="label text-muted">
